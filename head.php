faf87c5fdad098df46bc07d45131a2bc3ae108e5
Ariz Buditiono <arizbuno@gmail.com>
2017-05-05 23:58:01 +0700

    Finale

--- head.php
+++
@@ -1,7 +0,0 @@
-		<meta charset="utf-8">
-		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-		<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-		<link rel="stylesheet" type="text/css" href="materialize/css/materialize.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" type="text/css" href="src/css/style.css">
-		<title>Home | TOKOKEREN</title>
--- script.php
+++
@@ -1,6 +0,0 @@
-		<script src="src/js/jquery-3.1.1.min.js"></script>
-		<script src="materialize/js/materialize.js"></script>
-		<script>
-			$(".button-collapse").sideNav();
-			$(".indicator").css("background-color: black;");
-		</script>
