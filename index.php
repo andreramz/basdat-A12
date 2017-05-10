6dc838e31feaa9ede7418a412f01b55b97a988ff
Andre Ramadhani <andreramz28@gmail.com>
2017-05-10 16:12:04 +0700

    a

--- index.php
+++ index.php
@@ -206,8 +206,6 @@
 			</div>
 		</main>
 		<?php include("layout/footer.php"); ?>
-		<script src="src/js/jquery-3.1.1.min.js"></script>
-		<script src="materialize/js/materialize.js"></script>
 		<script>
 			$(".button-collapse").sideNav();
 			$(".indicator").css("background-color: black;");
