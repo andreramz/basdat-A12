88e6e27e6b0a7a2f80f5d1a6f43e156b2d3f738c
Aldi Fahrezi <Aldi Fahrezi>
2017-05-13 23:47:12 +0700

    Fix location

--- go_pembeli.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['role'] = 'pembeli';
-	$_SESSION['logged'] = 'Aldi';
-	$_SESSION['email'] = 'aborrett13@time.com';
-?>
--- go_penjual.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['role'] = 'penjual';
-	$_SESSION['logged'] = 'Aldi';
-	$_SESSION['email'] = 'aborrett13@time.com';
-?>
